705698a83274f5f1feb5ca2d1904ae25c3e8ecbe
JoM <jordan.marescaux@gmail.com>
2023-11-07 15:53:33 +0100

    work on card not finished

--- routes/cart.js
+++ routes/cart.js
@@ -4,10 +4,7 @@ var router = express.Router();
 const Cart = require('../models/cart');
 
 router.post('/', (req, res) => {
-			
-       
-    
-            const newCart = new Cart({
+			        const newCart = new Cart({
               departure: req.body.departure,
               arrival: req.body.arrival,
               //date: req.body.datedepart,
