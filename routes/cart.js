17fa752ac1831b63f38d1ab1d1018d8c1c6a4cdc
David <evil.dave59@gmail.com>
2023-11-08 14:27:06 +0100

    ajout dans la collection cart avec le book

--- routes/cart.js
+++ routes/cart.js
@@ -1,27 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Cart = require('../models/cart');
+const Cart = require("../models/cart");
 
-router.post('/', (req, res) => {
-			        const newCart = new Cart({
-              departure: req.body.departure,
-              arrival: req.body.arrival,
-              //date: req.body.datedepart,
-            });
-            newCart.save().then(data => {
-              res.json({ result: true, cart: data });
-            });
-          
-        
-  
-					
-					
-				});
-		
-
-
-
-
+router.post("/", (req, res) => {
+  const newCart = new Cart({
+    departure: req.body.departure,
+    arrival: req.body.arrival,
+    //date: req.body.datedepart,
+    price: req.body.price,
+  });
+  newCart.save().then((data) => {
+    res.json({ result: true, cart: data });
+  });
+  Cart.find().then((data) => {
+    console.log(data);
+  });
+});
 
 module.exports = router;
