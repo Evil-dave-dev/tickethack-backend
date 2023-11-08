4c02793b228ade9f19823deb261b6ecfec80851b
David <evil.dave59@gmail.com>
2023-11-08 10:54:38 +0100

    empty fields and result-content when click

--- routes/trips.js
+++ routes/trips.js
@@ -1,31 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Trip = require('../models/trips');
+const Trip = require("../models/trips");
 
-
-router.post('/', (req, res) => {
-
-	if(!req.body.departure || !req.body.arrival /*|| !req.body.datedepart*/) {
-    res.json({ result: false, error: 'missing or empty fields'});
-    return;
-  }
-
-	Trip.find({departure: req.body.departure, arrival: req.body.arrival/*, date: req.body.datedepart*/}).then(data => {
-	 
-		if (data) {
-			res.json({ result: true, trips: data});
-		} else {
-			res.json({ result: false, error: 'No trip found.'})
-		}
-	});
+router.post("/", (req, res) => {
+  Trip.find({
+    departure: req.body.departure,
+    arrival: req.body.arrival,
+    // date: req.body.date,
+  }).then((data) => {
+    if (data.length !== 0) {
+      res.json({ result: true, trips: data });
+    } else {
+      res.json({ result: false, error: "No trip found." });
+    }
+  });
 });
-
-
-
-
-
-
-
 
 module.exports = router;
